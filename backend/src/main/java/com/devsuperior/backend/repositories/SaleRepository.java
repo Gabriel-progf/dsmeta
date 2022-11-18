6cca0304f1886b3ce37086c33b6e1225f4332fe3
Gabriel <gabriel.ol.macedo@hotmail.com>
2022-11-18 20:11:25 -0300

    Fixed msg sending error

--- backend/src/main/java/com/devsuperior/backend/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/backend/repositories/SaleRepository.java
@@ -6,13 +6,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.devsuperior.backend.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale,Long> {
     
     @Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
-    Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
+    Page<Sale> findSales(@Param("min")LocalDate min, @Param("max")LocalDate max, Pageable pageable);
     
 
 }
